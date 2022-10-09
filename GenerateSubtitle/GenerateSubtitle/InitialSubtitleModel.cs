f3e087d9e3eded3799dad273e214ecb218b19f5a
JinLong <phachco@gmail.com>
2022-10-09 22:57:34 +0700

    commit all files

---
+++ GenerateSubtitle/GenerateSubtitle/InitialSubtitleModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateSubtitle
+{
+    public class InitialModel
+    {
+        public List<SubtitleModel> InitialSegments { get; set; }
+    }
+
+    public class SubtitleModel
+    {
+        public SubtitleDetail TranscriptSegmentRenderer { get; set; }
+    }
+
+    public class SubtitleDetail
+    {
+        public string StartMs { get; set; }
+        public string EndMs { get; set; }
+        public SnippetModel Snippet { get; set; }
+    }
+
+    public class SnippetModel
+    {
+        public List<TextSubtitleModel> Runs { get; set; }
+    }
+    public class TextSubtitleModel
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ GenerateSubtitle/GenerateSubtitle/Program.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace GenerateSubtitle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertNumberToTimeOfSubtitle();
+
+            Console.Read();
+        }
+        private static void ConvertNumberToTimeOfSubtitle()
+        {
+            //6319
+            //00:00:02,958 --> 00:00:05,541
+            using (StreamReader r = new StreamReader($"subtitle.json"))
+            {
+                string nameFile = Console.ReadLine();
+                string json = r.ReadToEnd();
+
+                var lines = JsonConvert.DeserializeObject<InitialModel>(json);
+                using StreamWriter file = new($"{nameFile}.srt");
+
+                int pos = 1;
+
+                foreach (var line in lines.InitialSegments)
+                {
+                    var data = line;
+
+                    string start = line.TranscriptSegmentRenderer.StartMs;
+                    string end = line.TranscriptSegmentRenderer.EndMs;
+                    string text = line.TranscriptSegmentRenderer.Snippet.Runs[0].Text;
+
+                    if (!string.IsNullOrEmpty(start) && !string.IsNullOrEmpty(end))
+                    {
+
+                        string time = String.Empty;
+
+                        if (start.Length <= 3)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            string milisecondStart = start.Substring(start.Length - 3);
+                            string secondStart = start.Substring(0, start.Length - 3);
+
+                            string milisecondEnd = end.Substring(end.Length - 3);
+                            string secondEnd = end.Substring(0, end.Length - 3);
+
+                            time = $"00:{FormatNumberToTime(secondStart)},{milisecondStart} --> 00:{FormatNumberToTime(secondEnd)},{milisecondEnd}";
+                        }
+
+                        file.WriteLine(pos);
+                        file.WriteLine(time);
+                        file.WriteLine(text.Replace("\n", " "));
+                        file.WriteLine("");
+
+                        pos += 1;
+                    }
+                }
+
+                file.Close();
+            }
+        }
+
+        private static string FormatNumberToTime(string num)
+        {
+            string formatCorrect = string.Empty;
+
+            int number = Convert.ToInt32(num);
+            //hour
+            //var hours = number / 360;
+            var minutes = number / 60;
+            var seconds = number % 60;
+
+            if (minutes == 0)
+            {
+                formatCorrect = $"00:{(seconds > 9 ? seconds : $"0{seconds}")}";
+            }
+            else
+            {
+                formatCorrect = $"{(minutes > 9 ? minutes : $"0{minutes}")}:{(seconds > 9 ? seconds : $"0{seconds}")}";
+            }
+
+            return formatCorrect;
+        }
+    }
+}
